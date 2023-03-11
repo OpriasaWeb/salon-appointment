7a09a4d7e9fe81e9fcc91e172b52cb7782ebf2cf
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-11 21:28:32 +0800

    Adding new service to the salon functioning with inserting it into database

---
+++ admin/add_service.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+require '../connect/db_connect.php';
+
+
+?>
+
+<!-- Include header -->
+<?php
+  include './includes/header.php';
+?>
+<!-- Include header -->
+
+<!-- Include navigation -->
+<?php
+  include './includes/nav.php';
+?>
+<!-- Include navigation -->
+
+  
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-12 mt-5">
+        <a href="./services.php" class="btn btn-secondary">Back</a>
+        <p class="fs-3 text-center">Add new service</p>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <form action="./code.php" method="POST">
+            <div class="mb-3">
+              <label for="">Service name</label>
+              <input type="text" name="service_name" class="form-control" require>
+            </div>
+            <div class="mb-3">
+              <label for="">Price</label>
+              <input type="text" name="service_price" class="form-control" require>
+            </div>
+            <div class="mb-3">
+              <label for="">Availability</label>
+              <select class="form-select form-select-sm mb-3" name="availability" aria-label=".form-select-sm example">
+                <option value="true" selected>Available</option>
+                <option value="true">Disable</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <button type="submit" name="add_service" class="btn btn-primary float-end">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<!-- Include navigation -->
+<?php
+  include './includes/footer.php';
+?>
+<!-- Include navigation -->
