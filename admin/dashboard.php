d78eb9a0b42157cee0bd08d8994e3dfbfd66ea73
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-11 13:46:41 +0800

    Login and heading directly to dashboard admin panel succeed.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,49 @@
 <?php
+session_start();
 
+require_once "../connect/db_connect.php";
 
 echo "Dashboard";
 
 ?>
+
+
+
+  <!-- Header includes -->
+  <?php
+    include 'includes/header.php';
+  ?>
+
+  <!-- Navigation includes -->
+  <?php
+    include 'includes/nav.php';
+  ?>
+
+
+
+  <div class="alert alert-warning alert-dismissible fade show mt-5" role="alert">
+      <strong>You are most welcome,</strong> <?= $_SESSION['message']; ?>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+
+  <!-- Prevent the going back after logout -->
+  <script type="text/javascript">
+    function preventBack(){
+        window.history.forward()
+    };
+    setTimeout("preventBack()", 0);
+    window.onunload=function(){
+        null;
+    }
+  </script>
+  <!-- Prevent the going back after logout -->
+
+
+  <!-- Footer includes -->
+  <?php
+    include 'includes/footer.php';
+  ?>
+
+
+</body>
+</html>
