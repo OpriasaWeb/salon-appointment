e8e56f1d381ef19d14aa4e75e4eb44aaa72e2e00
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-11 21:29:09 +0800

    feat: will be working on it tomorrow, not complete yet.

---
+++ admin/update_service.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+
+require '../connect/db_connect.php';
+
+
+?>
+
+<!-- Include header -->
+<?php
+  include './includes/header.php';
+?>
+<!-- Include header -->
+
+<!-- Include navigation -->
+<?php
+  include './includes/nav.php';
+?>
+<!-- Include navigation -->
+
+  
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-12 mt-5">
+        <a href="./services.php" class="btn btn-secondary">Back</a>
+        <p class="fs-3 text-center">Update the service</p>
+      </div>
+      <div class="card">
+        <div class="card-body">
+
+          <!-- Update mysqli code -->
+          <?php
+            if(isset($_GET['service_id'])){
+              $service_id = mysqli_real_escape_string($conn, $_GET['service_id']);
+              
+              // Update query sql
+              $update_query = "SELECT * FROM salon_services WHERE services_id = '$service_id' ";
+
+              // Run the query update sql
+              $update_run = mysqli_query($conn, $update_query);
+
+              if(mysqli_num_rows($update_run) > 0){
+                
+                $service = mysqli_fetch_array($update_run);
+                // print_r($service);
+
+              
+          
+          
+          ?>
+          <!-- Update mysqli code -->
+
+          <form action="./code.php" method="POST">
+            <div class="mb-3">
+              <label for="">Service name</label>
+              <input type="text" name="service_name" value="<?= $service['service_name'] ?>" class="form-control" require>
+            </div>
+            <div class="mb-3">
+              <label for="">Price</label>
+              <input type="text" name="service_price" value="<?= $service['price'] ?>" class="form-control" require>
+            </div>
+            <div class="mb-3">
+              <label for="">Availability</label>
+              <select class="form-select form-select-sm mb-3" name="availability" aria-label=".form-select-sm example">
+                <option value="
+                <?php
+                  if($service['availability'] == 'true'){
+                ?>
+                    <option value="true" selected>Available</option>
+                <?php
+                  } else{
+                    ?>
+                    <option value="false" selected>Disable</option>
+                  <?php
+                  }
+                
+
+                ?>
+                "></option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <button type="submit" name="update_service" class="btn btn-success float-end">Update</button>
+            </div>
+          </form>
+        </div>
+        
+        <?php
+            } else{
+              echo "No such service id found.";
+            }
+          }
+        ?>
+
+      </div>
+    </div>
+  </div>
+
+
+<!-- Include navigation -->
+<?php
+  include './includes/footer.php';
+?>
+<!-- Include navigation -->
