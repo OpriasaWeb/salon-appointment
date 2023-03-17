f0d32ec13e3e8dc69ad38166384bfb973129ebed
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-17 16:17:17 +0800

    Upgrade the modal information

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ session_start();
 // Database connection - PDO
 require_once './connect/db_connect.php';
 
-echo "Salon Appointment";
-
-
 ?>
 
 
@@ -23,12 +20,14 @@ echo "Salon Appointment";
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Are you an old or new customer?</h5>
+                <h5 class="modal-title">Enter your phone number</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <a href="" class="btn btn-primary">Old customer</a>
-                <a href="" class="btn btn-success ml-3">New customer</a>
+                <form action="./code.php" method="GET">
+                    <input type="text" class="form-control" name="book_phone" placeholder="09+" required>
+                    <button type="submit" name="enter_phone" class="btn btn-sm btn-primary float-end mt-3">Enter</button>
+                </form>
             </div>
             </div>
         </div>
