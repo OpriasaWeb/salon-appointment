26766e6d4a317aad2119b01e2069e660922f21d9
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-17 16:29:14 +0800

    Message file is for message session, require it each tab is highly suggested

---
+++ message.php
@@ -0,0 +1,12 @@
+<?php
+    if(isset($_SESSION['message'])):
+?>
+
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <?= $_SESSION['message']; ?>
+    </div>
+
+<?php
+    unset($_SESSION['message']);    
+    endif;
+?>
