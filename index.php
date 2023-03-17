dcea2ae6bb4c6e6b67fac55e4eb210ca9af43c74
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-17 17:20:35 +0800

    Modified due to debugging and to test the php code. Everything is good now.

--- index.php
+++ index.php
@@ -4,6 +4,47 @@ session_start();
 // Database connection - PDO
 require_once './connect/db_connect.php';
 
+if(isset($_GET['enter_phone'])){
+
+    $book_phone = mysqli_real_escape_string($conn, $_GET['book_phone']);
+
+    $query_book = "SELECT * FROM salon_customer WHERE phone = '$book_phone' ";
+
+    $result_query = mysqli_query($conn, $query_book);
+
+    // if($result_query){
+    //     $row = mysqli_num_rows($result_query);
+    //     if($row){
+    //         echo "<br>";
+    //         echo "<br>";
+    //         echo "<br>";
+    //         echo "<br>";
+    //         printf("Number of rows in the table: " . $row);
+    //     }
+        // mysqli_free_result($result_query);
+    // }
+
+    // $row = mysqli_num_rows($result_query);
+
+    // if($row){
+        // header("Location: old_customer.php");
+    //     printf("Number of row in the table : " . $row);
+    // } else{
+        // header("Location: new_customer.php");
+    // }
+
+    if(mysqli_num_rows($result_query) == 0){
+      $_SESSION['message'] = "Good day!";
+      $_SESSION['message'] = mysqli_num_rows($result_query);
+      header("Location: new_customer.php");
+    } else{
+      $_SESSION['message'] = "Good day! You can now book.";
+      $_SESSION['message'] = mysqli_num_rows($result_query);
+      header("Location: old_customer.php");
+      exit(0);
+    }
+}
+
 ?>
 
 
@@ -14,6 +55,14 @@ require_once './connect/db_connect.php';
     <!-- Include the nav.php -->
     <?php require_once './includes/nav.php' ?>
 
+    <?php
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    ?>
+
+    <!-- Message session -->
+    <!-- <?php require_once './message.php' ?> -->
 
     <!-- Modal -->
     <div class="modal" tabindex="-1" id="exampleModal"> <!-- ID modal depends on the id service from database -->
@@ -24,7 +73,7 @@ require_once './connect/db_connect.php';
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="./code.php" method="GET">
+                <form action="" method="GET">
                     <input type="text" class="form-control" name="book_phone" placeholder="09+" required>
                     <button type="submit" name="enter_phone" class="btn btn-sm btn-primary float-end mt-3">Enter</button>
                 </form>
