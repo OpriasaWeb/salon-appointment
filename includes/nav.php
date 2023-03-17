91a50d64ff84462b16cc527a42b4462d6f34d1ab
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-17 16:14:36 +0800

    Book appointment added to navigation so that when the user is new, they will redirect to new_customer wherein they shall register their account otherwise, they go to old_customer to book their appointment.

--- includes/nav.php
+++ includes/nav.php
@@ -13,22 +13,10 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Home</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Services
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/salon_appointment/service.php">Haircut woman - 150</a></li>
-              <li>
-                <button type="button" class="dropdown-item btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal</button>
-              </li>
-              <li><a class="dropdown-item" href="/salon_appointment/service.php">Haircut man - 100</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+          <li class="nav-item">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Book appointment</button>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
