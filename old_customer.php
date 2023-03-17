e6ed01c7ad3bda81d75cb53700923243f353f426
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-17 22:04:45 +0800

    The list services code is done and working

---
+++ old_customer.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+
+require './connect/db_connect.php';
+
+
+
+?>
+
+  <!-- Include the header.php -->
+  <?php require_once './includes/header.php' ?>
+
+  <!-- Message session -->
+  <!-- <?php require_once './message.php' ?> -->
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6 mt-5">
+        <p class="fs-3">Appointment booking</p>
+        <p class="fs-5">Hello, Jeremy!</p>
+        <form action="./code.php" method="POST">
+
+          <!-- Debugging - code later -->
+            <!-- Get rid of code.php in the user panel -->
+            <!-- Put all of the php code in the index file -->
+            <!-- So that the get method should work and get the info from the index to old_customer php file -->
+          <!-- Debugging - code later -->
+          
+          <!-- Get the id of the phone user -->
+          <div class="mb-3">
+            <input type="hidden" class="form-control" name="customer_id" id="">
+          </div>
+          <!-- ------------------------------------------------ -->
+          <!-- SERVICES -->
+          <div class="mb-3">
+            <label for="" class="form-label">Services</label>
+            <select class="form-select" aria-label="Default select example">
+            <option selected>Choose service</option>
+
+            <?php
+              // SQL query to get all from salon services data
+              $services_query = "SELECT * FROM salon_services";
+
+              // Run the query using mysqli_query
+              $services_run = mysqli_query($conn, $services_query);
+
+              // If the run query is greated than 1,
+              if(mysqli_num_rows($services_run) > 0){
+                // while there is data that are fetching, the condition is true
+                while($services = mysqli_fetch_array($services_run)){
+            ?>
+              <option value="<?php echo $services['services_id'] ?>"><?php echo $services['service_name'] ?> - <?php echo $services['price'] ?></option>
+            
+          <?php
+              }
+            }
+          ?>
+            </select>
+          </div>
+          <!-- SERVICES -->
+          <!-- ------------------------------------------------ -->
+          <div class="mb-3">
+            <label for="" class="form-label">Date of appointment</label>
+            <input type="date" class="form-control" name="phone_number" id="">
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Time of appointment</label>
+            <input type="time" class="form-control" name="phone_number" id="">
+          </div>
+          <a href="./index.php" class="btn btn-info">Back</a>
+          <button class="btn btn-primary float-end" name="book_appointment" type="submit">Book</button>
+        </form>
+      </div>
+      <div class="col-md-6">
+        <div class="image-reg">
+          <img src="./images/salon2.png" class="img-fluid " alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+  <!-- Include the footer.php -->
+  <?php require_once './includes/footer.php' ?>
