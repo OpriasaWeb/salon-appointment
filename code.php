edcf0870cd347f9158c72d5652d711fbdd3cb74b
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-18 10:33:21 +0800

    Booking an appointment with database is working and no error found so far.

--- code.php
+++ code.php
@@ -29,6 +29,34 @@ if(isset($_POST['register_acc'])){
 }
 // Register new customer account
 
+// Book an appointment
+if(isset($_POST['book_appointment'])){
+
+  $customer_id = mysqli_real_escape_string($conn, $_POST['customer_id']);
+  $service = mysqli_real_escape_string($conn, $_POST['service']);
+  $date_appointment = mysqli_real_escape_string($conn, $_POST['date_appointment']);
+  $time_appointment = mysqli_real_escape_string($conn, $_POST['time_appointment']);
+
+  $appointment_query = "INSERT INTO salon_appointments (customer_id, services_id, service_date, service_time) 
+                        VALUES ('$customer_id', '$service', '$date_appointment', '$time_appointment')";
+  
+  $appointment_run = mysqli_query($conn, $appointment_query);
+
+  if($appointment_run){
+    $_SESSION['message'] = "Successfully book your appointment.";
+    header("Location: index.php");
+    exit(0);
+  }else{
+    $_SESSION['message'] = "Failed to book. Please, try again.";
+    header("Location: old_customer.php");
+    exit(0);
+  }
+
+
+
+}
+// Book an appointment
+
 
 
 // Code - input from user
