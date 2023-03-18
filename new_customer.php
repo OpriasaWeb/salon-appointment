72d7b09360ca58ca57839818dc4a2dae9326ff11
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-03-18 10:38:55 +0800

    Change the position of the input and image for the sense of responsiveness of the website.

--- new_customer.php
+++ new_customer.php
@@ -12,29 +12,30 @@ require './connect/db_connect.php';
 
   <div class="container mt-5">
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-6 mt-5">
         <div class="image-reg">
-          <img src="./images/register.png" class="img-fluid " alt="">
+          <p class="fs-3">Register your account:</p>
+          <form action="./code.php" method="POST">
+            <div class="mb-3">
+              <label for="" class="form-label">Fullname</label>
+              <input type="text" class="form-control" name="fullname" id="">
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Email address</label>
+              <input type="email" class="form-control" name="email_address" id="">
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Phone no. #</label>
+              <input type="phone" class="form-control" name="phone_number" id="">
+            </div>
+            <a href="./index.php" class="btn btn-info">Back</a>
+            <button class="btn btn-success float-end" name="register_acc" type="submit">Register</button>
+          </form>
+          
         </div>
       </div>
-      <div class="col-md-6 mt-5">
-        <p class="fs-3">Register your account:</p>
-        <form action="./code.php" method="POST">
-          <div class="mb-3">
-            <label for="" class="form-label">Fullname</label>
-            <input type="text" class="form-control" name="fullname" id="">
-          </div>
-          <div class="mb-3">
-            <label for="" class="form-label">Email address</label>
-            <input type="email" class="form-control" name="email_address" id="">
-          </div>
-          <div class="mb-3">
-            <label for="" class="form-label">Phone no. #</label>
-            <input type="phone" class="form-control" name="phone_number" id="">
-          </div>
-          <a href="./index.php" class="btn btn-info">Back</a>
-          <button class="btn btn-success float-end" name="register_acc" type="submit">Register</button>
-        </form>
+      <div class="col-md-6">
+        <img src="./images/register.png" class="img-fluid " alt="">
       </div>
     </div>
   </div>
